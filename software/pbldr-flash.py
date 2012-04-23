1dee84c0fa71f71456bc463bc0547d0a93341107
Eric Evenchick <eric@evenchick.com>
2012-04-23 14:58:46 -0400

    first working state

--- software/pbldr-flash.py
+++ software/pbldr-flash.py
@@ -1,26 +1,26 @@
 import serial
+import binascii
 
-
-ser = serial.Serial(3, 115200, timeout=0.1)
+ser = serial.Serial(3, 115200, timeout=1)
 
 print "Opening port:", ser.portstr
 
 ready = False
 
-for i in range(0,100):
-    ser.write('S')               # request flash start
-    if (ser.readline() == "OK"): # check for response
+for i in range(0,10):
+    if (ser.readline() == "OK\n"): # check for response
         print "Starting Flash..."
         ready = True
         break;
-
 if not ready:
     print "No response from target!"
     quit()
 
 with open("file.hex", 'rb') as f:
-    byte = f.read(1)
-    while byte != "":
-        ser.write(byte)
-        print byte
-        byte = f.read(1)        # get the next byte
+    for line in f.readlines()[1:]:
+            for byte in binascii.unhexlify(line[9:len(line)-4]):
+                ser.write(byte)
+                print binascii.hexlify(byte)
+
+for i in range(1,1000):
+    ser.write("DONE")
