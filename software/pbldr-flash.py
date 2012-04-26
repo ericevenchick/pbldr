940c8b3fce31c445f198a86d3c01518d2dceadbe
Eric Evenchick <eric@evenchick.com>
2012-04-25 22:50:48 -0400

    flashes in 64 byte blocks, waits for ack

--- software/pbldr-flash.py
+++ software/pbldr-flash.py
@@ -15,17 +15,24 @@ print "Waiting for target..."
 for i in range(0,50):
     ser.write('S')
     if (ser.readline() == "OK\n"): # check for response
-        print "Starting Flash..."
+        print "Flashing target",
         ready = True
         break;
 if not ready:
     print "No response from target!"
     quit()
 
+count = 1
 with open("file.hex", 'rb') as f:
     for line in f.readlines():
             for byte in (line[9:len(line)-4]):
                 ser.write(byte)
+                if count == 64:
+                    ser.read(1)
+                    count = 1
+                    print ".",
+                else:
+                    count = count + 1
 
 for i in range(0,64):
     ser.write('D')
